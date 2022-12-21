d0c3284799deab0f21fda13f0fa543cd4d89164a
Chia-Ching Yen <60199319+ChiaChing-Yen@users.noreply.github.com>
2022-12-21 10:26:13 +0800

    add transformation convertion

--- compas2gh.py
+++ compas2gh.py
@@ -21,17 +21,40 @@ import Rhino.Geometry as rg  # type: ignore #noqa: F401
 __author__ = 'ChiaChing Yen'
 __email__ = '<ccyen@umich.edu>'
 
+def ct2rt(ct):
+    """Converts a compas transformation to a Rhino.Grasshopper transformation.
+    """
+    t = rg.Transform(1)
+    t.M00 = ct.list[0]
+    t.M01 = ct.list[1]
+    t.M02 = ct.list[2]
+    t.M03 = ct.list[3]
+    t.M10 = ct.list[4]
+    t.M11 = ct.list[5]
+    t.M12 = ct.list[6]
+    t.M13 = ct.list[7]
+    t.M20 = ct.list[8]
+    t.M21 = ct.list[9]
+    t.M22 = ct.list[10]
+    t.M23 = ct.list[11]
+    t.M30 = ct.list[12]
+    t.M31 = ct.list[13]
+    t.M32 = ct.list[14]
+    t.M33 = ct.list[15]
+
+    return t
+
 
 def convert_compas_geometry_to_gh_geometry(compas_geometry):
-    """Converts a Rhino point to a COMPAS point.
+    """Converts a compas geometry to a Rhino.Grasshopper geometry.
 
     Parameters
     ----------
-        rhino_point (Any): The rhino geometry that will be converted
+        compas_geometry (Any): The compas geometry that will be converted
 
     Returns:
     ----------
-        compas.geometry
+        Rhino.Geometry
     """
     if isinstance(compas_geometry, cg.Point):
         return cgha.PointArtist(compas_geometry).draw()
@@ -47,6 +70,8 @@ def convert_compas_geometry_to_gh_geometry(compas_geometry):
         return cgha.MeshArtist(compas_geometry).draw()
     elif isinstance(compas_geometry, cg.Polyline):
         return cgha.PolylineArtist(compas_geometry).draw()
+    elif isinstance(compas_geometry, cg.Transform):
+        return ct2rt(compas_geometry)
     else:
         print("{0} is not supportted.".format(type(compas_geometry)))
         raise TypeError
